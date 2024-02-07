639599088da5fa52b78260dd2d4d094ce2eed18d
Alexandr Plotnikov <alexandrplotnikoff322@gmail.com>
2024-02-07 18:15:16 +0200

    [File upload] File upload logic implemented

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import fs from 'fs';
 import cors from 'cors';
+import multer from 'multer';
 import dotenv from 'dotenv';
 
 import { loginValidation, postCreateValidation, registerValidation } from './validation.js';
@@ -17,14 +19,33 @@ mongoose
 
 const app = express();
 
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    if (!fs.existsSync('uploads')) {
+      fs.mkdirSync('uploads');
+    }
+    cb(null, 'uploads');
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
 app.use(express.json());
 app.use(cors());
+app.use('/uploads', express.static('uploads'));
 
 app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
 app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register);
 app.get('/auth/me', checkAuth, UserController.getMe);
 
-app.get('/tags', PostController.getLastTags);
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  });
+});
 
 app.get('/posts', PostController.getAll);
 app.get('/posts/tags', PostController.getLastTags);
